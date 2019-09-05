1d6bcfa9dc17435b6ff48ad3a8240bb762718cc6
Tomaras <mkskoly@gmail.com>
2019-09-04 22:41:20 -0500

    basic file structure and images

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import Card from "./components/Card";
+
+class App extends Component {
+  state = {
+    
+  }
+}
+
+export default App;
---
+++ client/src/components/Header/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+function Header(props) {
+  return <div></div>;
+}
+
+export default Header;
