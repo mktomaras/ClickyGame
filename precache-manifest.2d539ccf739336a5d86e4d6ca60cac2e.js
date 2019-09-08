3b1236b7b7f52461f9394980cd3179a88e475bb5
Tomaras <mkskoly@gmail.com>
2019-09-07 21:17:44 -0500

    Updates

---
+++ precache-manifest.2d539ccf739336a5d86e4d6ca60cac2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb14603aaa49efd3d79fef9eded64989",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "b0919e983376288dc41d",
+    "url": "/ClickyGame/static/css/main.69015449.chunk.css"
+  },
+  {
+    "revision": "606b67ae68288e2d616e",
+    "url": "/ClickyGame/static/js/2.5f124323.chunk.js"
+  },
+  {
+    "revision": "b0919e983376288dc41d",
+    "url": "/ClickyGame/static/js/main.a189ecf7.chunk.js"
+  },
+  {
+    "revision": "3f94739a733620af8917",
+    "url": "/ClickyGame/static/js/runtime~main.1df70f2f.js"
+  },
+  {
+    "revision": "da5c47da99f4acb50b7433c7db433ca7",
+    "url": "/ClickyGame/static/media/astronomy-constellations-milky-way-127577.da5c47da.jpg"
+  }
+]);
